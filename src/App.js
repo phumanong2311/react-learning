a62fe25265eb70859faf8caaf66e9f5b237a4347
kieran234 <kieran.nguyen@watasolutions.com>
2022-06-15 12:40:27 +0700

    apply tailwind

--- src/App.js
+++ src/App.js
@@ -14,13 +14,20 @@ const App = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error}</p>;
 
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    {data.rates.map(({ currency, rate }) => (
+      <div key={currency}>
+        <p>
+          {currency}: {rate}
+        </p>
+      </div>
+    ))}
+    </>
+  )
 }
 
 export default App;
